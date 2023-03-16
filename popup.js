26537c7c686635dc6f741454598255f107045ad0
majestictree <unlimited.blade.works22@gmail.com>
2023-03-16 11:25:35 +0900

    option系wo 削除

--- popup.js
+++ popup.js
@@ -7,10 +7,13 @@
 // });
 
 chrome.tabs.query({ active: true, currentWindow: true},
-  (tab) => chrome.scripting.executeScript({
+  (tab) => {
+    chrome.scripting.executeScript({
     target: { tabId: tab[0].id },
     function: reddenPage
-}));
+    })
+  }
+);
 
 function reddenPage() {
   document.body.style.backgroundColor = 'red';
