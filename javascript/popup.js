7b512e60c9c9d4ecba7cbdfabf41768e042fac5f
majestictree <unlimited.blade.works22@gmail.com>
2023-03-29 10:23:38 +0900

    update: 茂木さんへ共有

--- javascript/popup.js
+++ javascript/popup.js
@@ -1,12 +1,47 @@
-document.getElementById("btn").addEventListener("click", async () => {
+calc();
+
+let reset = function () {
+    danger_counter = 0;
+    warning_counter = 0;
+    white_counter = 0;
+    localStorage.setItem("danger", danger_counter);
+    localStorage.setItem("warning", warning_counter);
+    localStorage.setItem("white", white_counter);
+    document.getElementById("danger").innerText = danger_counter;
+    document.getElementById("warning").innerText = warning_counter;
+    document.getElementById("white").innerText = white_counter;
+}
+
+async function calc() {
+  let danger_counter = Number(localStorage.getItem("danger")) || 0;
+  let warning_counter = Number(localStorage.getItem("warning")) || 0;
+  let white_counter = Number(localStorage.getItem("white")) || 0;
+    
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: onRun,
+    func:parseDOM
+  }).then(function(r) {
+    for (const {frameId, result} of r) {
+      danger_counter += result.danger;
+      warning_counter += result.warning;
+      white_counter += result.white;
+    }
+    document.getElementById("danger").innerText = danger_counter;
+    document.getElementById("warning").innerText = warning_counter;
+    document.getElementById("white").innerText = white_counter;
+    localStorage.setItem("danger", danger_counter);
+    localStorage.setItem("warning", warning_counter);
+    localStorage.setItem("white", white_counter);
   });
-});
+}
 
-let onRun = function() {
-  let count = document.getElementsByTagName('li').length;
-  console.log(count);
-};
+function parseDOM() {
+  let danger = document.querySelectorAll(".bg-danger").length;
+  let warning = document.querySelectorAll(".bg-warning").length;
+  let white = document.querySelectorAll("tbody>tr").length;
+  return {danger, warning, white};
+}
+
+document.getElementById("reset").onclick = reset;
